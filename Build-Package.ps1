49e4cdc41079029e2e9088cbbef3163b4d67d6af
AngelcoMilk-天使棉 <2326412638@qq.com>
2026-07-25 17:58:47 +0800

    Flatten repo structure, fix paths

--- Build-Package.ps1
+++ Build-Package.ps1
@@ -4,7 +4,7 @@ Push-Location $root
 try {
     dotnet tool restore
     dotnet build .\SaveGuard.csproj -c Release
-    dotnet run --project ..\SaveGuard.Tests\SaveGuard.Tests.csproj -c Release
+    dotnet run --project .\SaveGuard.Tests\SaveGuard.Tests.csproj -c Release
     New-Item -ItemType Directory -Force .\dist | Out-Null
     Copy-Item .\bin\Release\SaveGuard.dll .\dist\SaveGuard.dll -Force
     Copy-Item .\CHANGELOG.md .\dist\CHANGELOG.md -Force
